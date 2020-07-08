15960b2d4131a3eecd753afe4863b3b004dbbe27
Jonathan Brumley <cayasso@gmail.com>
2020-07-07 19:19:56 -0600

    fix deploy sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ "${CIRCLE_BRANCH}" == "production" ]; then
-  netlify deploy -p -a ${NETLIFY_TOKEN} -s ${NETLIFY_PROD_SITE_ID} -d ./build
+  netlify deploy -p -a ${NETLIFY_TOKEN} -s ${NETLIFY_PROD_SITE_ID} --prod
 elif [ "${CIRCLE_BRANCH}" == "master" ]; then
-  netlify deploy -p -a ${NETLIFY_TOKEN} -s ${NETLIFY_DEV_SITE_ID} -d ./build
+  netlify deploy -p -a ${NETLIFY_TOKEN} -s ${NETLIFY_DEV_SITE_ID} --prod
 else
   echo "Environment not found!"
   exit 1
