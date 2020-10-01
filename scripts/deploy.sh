52d78351a645e24f21cdecab144a22a13cce80be
Rodimiro Cerrato E <rodyce@gmail.com>
2020-09-30 22:37:54 -0600

    Modify deploy script to use within Circle CI
    Signed-off-by: Rodimiro Cerrato E <rodyce@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
 
+readonly SERVERLESS_DIR=.serverless
+readonly SERVERLESS_YML=serverless.yml
+readonly SERVERLESS_TEMPLATES_DIR=.serverless_templates
+
+# Ensure SERVERLESS_DIR exists and it is empty.
+if [[ ! -d $SERVERLESS_DIR ]]; then
+  mkdir $SERVERLESS_DIR
+else
+  rm -f $SERVERLESS_DIR/*
+fi
+
 if [ "${CIRCLE_BRANCH}" == "production" ]; then
-  netlify deploy -p -a ${NETLIFY_TOKEN} -s ${NETLIFY_PROD_SITE_ID} --prod
+  cp $SERVERLESS_TEMPLATES_DIR/prod/* $SERVERLESS_DIR
+  cp prod-$SERVERLESS_YML $SERVERLESS_YML
+  npx serverless
 elif [ "${CIRCLE_BRANCH}" == "master" ]; then
-  netlify deploy -p -a ${NETLIFY_TOKEN} -s ${NETLIFY_DEV_SITE_ID} --prod
+  cp $SERVERLESS_TEMPLATES_DIR/dev/* $SERVERLESS_DIR
+  cp dev-$SERVERLESS_YML $SERVERLESS_YML
+  npx serverless
 else
   echo "Environment not found!"
   exit 1
+fi
+
+# Cleanup serverless yml file.
+if [[ -f $SERVERLESS_YML ]]; then
+  rm -f $SERVERLESS_YML
 fi
