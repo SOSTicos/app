7ef66edf3d7b60c34bd78518e8425917a2992632
Rodimiro Cerrato E <rodyce@gmail.com>
2020-11-12 22:50:09 -0600

    Fix deploy script to fail with non-zero exit code
    Signed-off-by: Rodimiro Cerrato E <rodyce@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,17 @@ readonly DEV_SERVERLESS_YML=dev-serverless.yml
 readonly PROD_SERVERLESS_YML=prod-serverless.yml
 readonly SERVERLESS_TEMPLATES_DIR=.serverless_templates
 
+invoke_serverless () {
+  npx serverless
+  if [ $? -eq 0 ]
+  then
+    echo "SUCCESS"
+  else
+    echo "AWS serverless deployment failed. Exit Code $?" >&2
+    exit 1
+  fi
+}
+
 # Ensure SERVERLESS_DIR exists and it is empty.
 if [[ ! -d $SERVERLESS_DIR ]]; then
   mkdir $SERVERLESS_DIR
@@ -16,11 +27,11 @@ fi
 if [ "${CIRCLE_BRANCH}" == "production" ]; then
   cp $SERVERLESS_TEMPLATES_DIR/prod/* $SERVERLESS_DIR
   cp $PROD_SERVERLESS_YML $SERVERLESS_YML
-  npx serverless
+  invoke_serverless
 elif [ "${CIRCLE_BRANCH}" == "master" ]; then
   cp $SERVERLESS_TEMPLATES_DIR/dev/* $SERVERLESS_DIR
   cp $DEV_SERVERLESS_YML $SERVERLESS_YML
-  npx serverless
+  invoke_serverless
 else
   echo "Environment not found!"
   exit 1
